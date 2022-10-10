06084caae7b030f06f85415b0e493d585735181c
Stefan Stax <stefanstaxbusiness@gmail.com>
2022-10-10 18:53:23 +0200

    Cookie IMP WIP

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -2,8 +2,8 @@ let cookieFooter = document.querySelector(".cky-footer-wrapper");
 let cookieLink = cookieFooter.children[2].children[0];
 let cookieIcon = document.querySelector(".cky-btn-revisit");
 
-window.onload = function () {
+(function changeCookieIMP() {
   cookieLink.innerHTML = "Stefan Stax";
   cookieLink.href = "https://staxy.io";
   cookieIcon.innerHTML = `<iconify-icon icon="akar-icons:settings-horizontal"></iconify-icon>`;
-};
+})();
