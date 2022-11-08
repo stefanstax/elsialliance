dfc858a8f622c927054e8a3857d93df3a457f14a
Stefan Stäx <stefanstax@Stefans-MacBook-Air-2.local>
2022-11-08 09:42:29 +0100

    Section title block link capabilities

--- blocks/section-title/block.php
+++ blocks/section-title/block.php
@@ -2,7 +2,9 @@
   <div class="flex gap-[10px] items-center">
     <!-- ! You're a F legend for making it this way -->
     <iconify-icon width="18" height="18" icon="akar-icons:<?php block_field("section-icon"); ?>"></iconify-icon>
-    <h2><?php block_field("section-title"); ?></h2>
+    <a href="<?php block_field("section-link");?>">
+      <h2><?php block_field("section-title"); ?></h2>
+    </a>
   </div>
   <h4><?php block_field("section-subtitle"); ?></h4>
   <p><?php block_field("section-description"); ?></p>
